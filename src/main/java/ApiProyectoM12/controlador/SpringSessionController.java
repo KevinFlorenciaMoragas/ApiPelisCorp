ac4bebc94d84fd8f22c4f0ae9bdf06373b3caa1e
KevinFlorenciaMoragas <120371012+KevinFlorenciaMoragas@users.noreply.github.com>
2023-03-28 19:32:16 +0200

    28/03

--- src/main/java/ApiProyectoM12/controlador/SpringSessionController.java
+++ src/main/java/ApiProyectoM12/controlador/SpringSessionController.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,24 +20,25 @@ import java.util.List;
 public class SpringSessionController {
     private final UserRepository userRepository;
     @PostMapping("/login")
-    public String login(@RequestBody User user, HttpServletRequest request, Model model) {
+    public ResponseEntity<?> login(@RequestBody User user, HttpServletRequest request, Model model) {
         System.out.println("Entramos en login");
         try {
-            if (userRepository.findByUsernameAndPassword(user.getUsername(),user.getPassword()) != null) {
+            User userExist = userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+            if (userExist!= null) {
                 System.out.println("Usuario correcto");
                 HttpSession session = request.getSession();
                 session.setAttribute("username", user.getUsername());
                 session.setAttribute("rol", user.getUserRol());
-                System.out.println(session.getAttribute(user.getUsername()));
-                return "redirect:/";
+                System.out.println(session.getAttribute("username"));
+                return ResponseEntity.ok(userExist);
             } else {
                 model.addAttribute("error", "Usuario o contraseña incorrectos");
                 System.out.println("Usuario incorrecto");
-                return "login";
+                return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario o contraseña incorrectos");
             }
         } catch (Exception e) {
             model.addAttribute("error", "Usuario o contraseña incorrectos");
-            return "login";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor");
         }
     }
     @GetMapping("/logout")
@@ -44,6 +47,7 @@ public class SpringSessionController {
         session.invalidate();
         return "redirect:/";
     }
+
     @GetMapping("/login")
     public String login(@RequestParam (required = false) String error, Model model) {
         if(error!= null)
